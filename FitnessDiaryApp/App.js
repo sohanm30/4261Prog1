b54041127b0dcf1994a12cf3ef304344041e3684
sohanm04 <84931341+sohanm04@users.noreply.github.com>
2025-01-23 13:42:04 -0500

    Expo Init for React Native App

---
+++ FitnessDiaryApp/App.js
@@ -0,0 +1,24 @@
+// App.js
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Hello, Fitness !</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+});
